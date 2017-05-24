31a273e50b148797e6fa48917d0ccf6161576005
Paul <paulll.lucas@gmail.com>
2017-05-24 04:54:20 -0700

    Add doc-strings

--- raven.cljs
+++ raven.cljs
@@ -9,25 +9,33 @@
 (def current-node (atom 2))
 (def current-buffer (atom 0))
 
-(defn send-msg [msg]
+(defn send-msg
+  "Encode OSC message & send to SCSynth"
+  [msg]
   (let [buf (.encode oscmsg (clj->js msg))]
     (.send socket buf 0 (.-length buf) "57110" "127.0.0.1")))
 
-(defn nest []
+(defn nest
+  "Create group on node 1"
+  []
   (send-msg {:address "/g_new"
              :args [{:type "integer" :value 1}
                     {:type "integer" :value 1}
                     {:type "integer" :value 0}]})
   "Building a nest on a distant power line...")
 
-(defn find-type [val]
+(defn find-type
+  "Return type-specific hash-map depending on value"
+  [val]
   (cond
     (integer? val) {:type "integer" :value val}
     (float? val) {:type "float" :value val}
     (string? val) {:type "string" :value val}
     :else {:type "integer" :value 0}))
 
-(defn lay [synthdef & args]
+(defn lay
+  "Create a new synth"
+  [synthdef & args]
   (swap! current-node inc)
   (send-msg {:address "/s_new"
              :args (into [{:type "string" :value synthdef}
@@ -38,19 +46,25 @@
   (println "An egg is hatching...")
   @current-node)
 
-(defn croak [node k v]
+(defn croak
+  "Set a node's control value"
+  [node k v]
   (send-msg {:address "/n_set"
              :args [{:type "integer" :value node}
                     {:type "string" :value k}
                     {:type "integer" :value v}]})
   "Croaking...")
 
-(defn fly []
+(defn fly
+  "Free node 1"
+  []
   (send-msg {:address "/n_free"
              :args [{:type "integer" :value 1}]})
   "Abandoning nest...")
 
-(defn load-buf [path]
+(defn load-buf
+  "Allocate buffer & read sound file"
+  [path]
   (swap! current-buffer inc)
   (send-msg {:address "/b_allocRead"
              :args [{:type "integer" :value @current-buffer}
@@ -58,7 +72,9 @@
                     {:type "integer" :value 0}
                     {:type "integer" :value 0}]}))
 
-(defn samples [dir]
+(defn samples
+  "Load a directory of sound files"
+  [dir]
   (.readdir
    fs dir
    (fn [err files]
@@ -66,12 +82,16 @@
        (println "IO Error: Unable to find directory")
        (doall (map #(load-buf %) files))))))
 
-(defn load-defs []
+(defn load-defs
+  "Load a directory of synth definitions"
+  []
   (send-msg {:address "/d_loadDir"
              :args [{:type "string"
                      :value (str (.cwd process) "/synths/")}]}))
 
-(defn compile []
+(defn compile
+  "Format & compile synth definitions in synthDefs.scd (via SCLang)"
+  []
   (let [replace-newline #(clojure.string.replace % #"\r\n|\n|\r" "")
         writedef-str (str "writeDefFile(\"" (.cwd process) "/synths/\")")
         replace-deffile #(clojure.string.replace % #"writeDefFile" writedef-str)]
@@ -83,11 +103,15 @@
                              (replace-deffile))]
            (child-process.exec (str "echo '" synthdefs "' | sclang"))))))))
 
-(defn reload-defs []
+(defn reload-defs
+  "Compile & reload synth definitions"
+  []
   (compile)
   (load-defs))
 
-(defn dir-check []
+(defn dir-check
+  "If synths directory doesn't exist, create it"
+  []
   (let [dir-path (str (.cwd process) "/synths/")
         mkdir-err #(when % (throw (js/Error. "IO Error: Unable to create directory")))
         access-err #(when % (.mkdir fs dir-path mkdir-err))]
