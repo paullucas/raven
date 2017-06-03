8746ef834929761fa7d133043486887c8ed0c0da
Paul <paulll.lucas@gmail.com>
2017-06-02 17:40:18 -0700

    Begin adding conspiracy

--- raven.cljs
+++ raven.cljs
@@ -1,37 +1,48 @@
 (require '[cljs.nodejs :as nodejs])
-(def fs (nodejs/require "fs"))
-(def process (nodejs/require "process"))
-(def child-process (nodejs/require "child_process"))
-(def dgram (nodejs/require "dgram"))
-(def oscmsg (nodejs/require "osc-msg"))
 
-(def socket (.createSocket dgram "udp4"))
-(def current-node (atom 2))
-(def current-buffer (atom 0))
+(def fs              (nodejs/require "fs"))
+(def process         (nodejs/require "process"))
+(def child-process   (nodejs/require "child_process"))
+(def dgram           (nodejs/require "dgram"))
+(def oscmsg          (nodejs/require "osc-msg"))
+(def io              (nodejs/require "socket.io-client"))
 
-(defn timestamp []
-  (.toISOString (new js/Date)))
+(def udp-socket      (atom (.createSocket dgram "udp4")))
+(def io-socket       (atom nil))
+(def current-node    (atom 2))
+(def current-buffer  (atom 0))
+(def current-session (atom []))
 
 (defn io-error [err]
   (when err
     (println "IO Error")))
 
-(def current-session-path (str (.cwd process) "/events/session" (timestamp) ".json"))
-(def current-session (atom []))
 
-(add-watch current-session
-           :fs-sync
-           (fn [_ _ _ state]
-             (.writeFile fs current-session-path state io-error)))
+(defn timestamp []
+  (.toISOString (new js/Date)))
+
+
+(def current-session-path
+  (str (.cwd process) "/events/session" (timestamp) ".json"))
+
+
+(defn add-session-watch []
+  (add-watch current-session
+             :fs-sync
+             (fn [_ _ _ state]
+               (.writeFile fs current-session-path state io-error))))
+
 
 (defn json->clj [data]
   (-> (.parse js/JSON data)
       (js->clj :keywordize-keys true)))
 
+
 (defn clj->json [data]
   (->> data
        clj->js
        (.stringify js/JSON)))
+
 
 (defn store-event
   "Append timestamp to event & conj to current-session atom"
@@ -39,11 +50,13 @@
   (->> (conj event {:timestamp (timestamp)})
        (swap! current-session conj)))
 
+
 (defn send-msg
   "Encode OSC message & send to SCSynth"
   [msg]
   (let [buf (.encode oscmsg (clj->js msg))]
-    (.send socket buf 0 (.-length buf) "57110" "127.0.0.1")))
+    (.send @udp-socket buf 0 (.-length buf) "57110" "127.0.0.1")))
+
 
 (defn nest
   "Create group on node 1"
@@ -55,6 +68,7 @@
                     {:type "integer" :value 0}]})
   "Building a nest on a distant power line...")
 
+
 (defn find-type
   "Return type-specific hash-map depending on value"
   [val]
@@ -64,29 +78,39 @@
     (string? val) {:type "string" :value val}
     :else {:type "integer" :value 0}))
 
-(defn lay
-  "Create a new synth"
-  [synthdef & args]
-  (swap! current-node inc)
-  (store-event {:type "lay"})
-  (send-msg {:address "/s_new"
-             :args (into [{:type "string" :value synthdef}
-                          {:type "integer" :value @current-node}
-                          {:type "integer" :value 1}
-                          {:type "integer" :value 1}]
-                         (mapv find-type args))})
-  (println "An egg is hatching...")
-  @current-node)
+
+(let [lay-fn (fn [synthdef & args]
+               (swap! current-node inc)
+               (store-event {:type "lay"
+                             :synthdef synthdef
+                             :args args})
+               (send-msg {:address "/s_new"
+                          :args (into [{:type "string" :value synthdef}
+                                       {:type "integer" :value @current-node}
+                                       {:type "integer" :value 1}
+                                       {:type "integer" :value 1}]
+                                      (mapv find-type args))})
+               (println "An egg is hatching...")
+               @current-node)]
+  (defn lay
+    "Create a new synth"
+    ([synthdef args] (lay-fn synthdef args))
+    ([synthdef & args] (lay-fn synthdef args))))
+
 
 (defn croak
   "Set a node's control value"
   [node k v]
-  (store-event {:type "croak"})
+  (store-event {:type "croak"
+                :node node
+                :key k
+                :value v})
   (send-msg {:address "/n_set"
              :args [{:type "integer" :value node}
                     {:type "string" :value k}
                     {:type "integer" :value v}]})
   "Croaking...")
+
 
 (defn fly
   "Free node 1"
@@ -96,6 +120,7 @@
              :args [{:type "integer" :value 1}]})
   (swap! current-node #(-> 2))
   "Abandoning nest...")
+
 
 (defn load-buf
   "Allocate buffer & read sound file"
@@ -107,6 +132,7 @@
                     {:type "integer" :value 0}
                     {:type "integer" :value 0}]}))
 
+
 (defn samples
   "Load a directory of sound files"
   [dir]
@@ -114,8 +140,9 @@
    fs dir
    (fn [err files]
      (if err
-       (println "IO Error: Unable to find directory")
+       (io-error err)
        (doall (map load-buf files))))))
+
 
 (defn load-defs
   "Load a directory of synth definitions"
@@ -123,6 +150,7 @@
   (send-msg {:address "/d_loadDir"
              :args [{:type "string"
                      :value (str (.cwd process) "/synths/")}]}))
+
 
 (defn compile
   "Format & compile synth definitions in synthDefs.scd (via SCLang)"
@@ -139,19 +167,21 @@
                               replace-deffile)]
            (child-process.exec (str "echo '" synthdefs "' | sclang"))))))))
 
+
 (defn reload-defs
   "Compile & reload synth definitions"
   []
   (compile)
   (load-defs))
 
+
 (defn dir-check
   "If synths directory doesn't exist, create it"
   []
   (let [dir-path (str (.cwd process) "/synths/")
-        mkdir-err #(when % (throw (js/Error. "IO Error: Unable to create directory")))
-        access-err #(when % (.mkdir fs dir-path mkdir-err))]
+        access-err #(when % (.mkdir fs dir-path io-error))]
     (.access fs dir-path fs.constants.F_OK access-err)))
+
 
 (defn session-check
   "If session file doesn't exist, create it"
@@ -159,6 +189,23 @@
   (try (.accessSync fs current-session-path fs.constants.F_OK)
        (catch :default err (.appendFileSync fs current-session-path "[]"))))
 
+
+(defn on-external-event [data]
+  (case (:type data)
+    "lay" (lay (:synthdef data) (:args data))
+    "croak" (croak (:node data) (:key data) (:value data))
+    :else nil))
+
+
+(defn join-conspiracy [address port]
+  (reset! io-socket (.connect io address (clj->js {:port port})))
+  (.on @io-socket "event" on-external-event)
+  (add-watch current-session
+             :conspiracy-sync
+             (fn [_ _ _ state]
+               (.emit @io-socket "event" (last state)))))
+
 (dir-check)
 (reload-defs)
 (session-check)
+(join-conspiracy "localhost" 3000)
